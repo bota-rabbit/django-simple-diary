746b066778e5f62e9846b4e9800455f6351a552d
bota-rabbit <rabbit_one75@yahoo.co.jp>
2026-01-02 11:48:31 +0900

    Setting up the admin site

--- diary/admin.py
+++ diary/admin.py
@@ -4,3 +4,6 @@ from .models import Diary
 @admin.register(Diary)
 class DiaryAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at', 'updated_at')
+    ordering = ('-created_at',)    # 並び順を指定
+    search_fields = ('title', 'body')    # 検索できるようにする
+    list_filter = ('created_at',)    # 日付で絞り込み
--- diary/models.py
+++ diary/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 class Diary(models.Model):
-    title = models.CharField(max_length=200)
-    body = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField("タイトル", max_length=200)
+    body = models.TextField("本文")
+    created_at = models.DateTimeField("作成日時", auto_now_add=True)
+    updated_at = models.DateTimeField("更新日時", auto_now=True)
 
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
