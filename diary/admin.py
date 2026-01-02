23c7bdd342c87750709bb3366212dbb2468692b5
bota-rabbit <rabbit_one75@yahoo.co.jp>
2026-01-02 10:51:49 +0900

    Setting up the admin site

--- diary/admin.py
+++ diary/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Diary
 
-# Register your models here.
+@admin.register(Diary)
+class DiaryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at')
